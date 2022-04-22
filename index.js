a92eb3fea19971851b35f890eb225d25a60179ec
LeFede <lefedeok@gmail.com>
2022-04-22 21:15:00 +0200

    Added post model

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 import { ApolloServer } from "apollo-server";
 import gql from 'graphql-tag'
+import mongoose from 'mongoose'
+
+import Post from './models/Post.js'
+import {} from 'dotenv/config'
+
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    password: String!
+    email: String!
+    createdAt: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World' 
+    getPosts: async () => {
+      try {
+        const posts = await Post.find({})
+        return posts
+      } catch (err) {
+        throw new Error(err)
+      }
+    }
   }
 }
 
@@ -18,5 +45,25 @@ const server = new ApolloServer({
   resolvers
 })
 
-server.listen({ port: process.env.PORT  || 4000 })
-  .then(({url}) => console.log(`Server running at ${url}`))
+
+const connectionString = `mongodb+srv://fede:${process.env.DB_PASSWORD}@cluster9.lua1z.mongodb.net/merng?retryWrites=true&w=majority`
+
+
+const connect = async () => {
+
+  const databaseConnection = await mongoose.connect(connectionString)
+  console.log(`Database Connected`)
+
+  const {url} = await server.listen({ port: process.env.PORT || 4000 })
+  console.log(`Server\'s running at ${url}`)
+
+}
+
+connect()
+
+// mongoose.connect(connectionString)
+//   .then(()=> {
+//     server.listen({ port: process.env.PORT || 4000 })
+//       .then(({ url }) => console.log(`Server running at ${url}`))
+//   })
+
